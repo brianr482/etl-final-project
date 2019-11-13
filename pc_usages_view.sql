2809e4fe234971584237efe9857ad68d443def07
ferch5003 <ferch5003@gmail.com>
2019-11-12 23:51:15 -0500

    Realizado segunda pregunta de negocio

--- pc_usages_view.sql
+++ pc_usages_view.sql
@@ -1,33 +1,11 @@
 CREATE VIEW `pc_usages_view` AS
     SELECT
-        dschool.SEDE_NOMBRE AS 'COLEGIO',
-        dschool.SEDE_CODIGO,
 		ddep.DEPTO AS 'DEPARTAMENTO',
-        dcity.MUNI AS 'MUNICIPIO',
-        darea.AREA_NOMBRE AS 'AREA',
-        dperiod.PERIODO_ANIO AS 'AÑO',
-        dcharacter.CARACTER_NOMBRE AS 'CARACTER',
-        delevel.NIVELENSE_NOMBRE AS 'NIVEL_DE_ENSEÑANZA',
-        dsday.JORNADA_NOMBRE AS 'JORNADA',
-        dpu.ENSEQ_NOMBRE AS 'ENSEQ_NOMBRE',
+        dpu.ENSEQ_NOMBRE AS `ENSEQ_NOMBRE`,
         fact.PC_USAGES_BY_SCCHOOL_AT_DEPARTMENT AS 'EQUIPOS DE CÓMPUTO'
     FROM
         world.fact_pc_usage fact
 			LEFT JOIN
 		world.dim_department ddep ON ddep.COD_KEY = fact.DEPARTMENT_KEY
             LEFT JOIN
-        world.dim_city dcity ON dcity.COD_KEY = fact.CITY_KEY
-            LEFT JOIN
-        world.dim_school dschool ON dschool.COD_KEY = fact.SCHOOL_KEY
-            LEFT JOIN
-        world.dim_area darea ON darea.COD_KEY = fact.AREA_KEY
-            LEFT JOIN
-        world.dim_period dperiod ON dperiod.COD_KEY = fact.PERIOD_KEY
-            LEFT JOIN
-        world.dim_character dcharacter ON dcharacter.COD_KEY = fact.CHARACTER_KEY
-            LEFT JOIN
-        world.dim_education_level delevel ON delevel.COD_KEY = fact.EDUCATION_LEVEL_KEY
-            LEFT JOIN
-        world.dim_school_day dsday ON dsday.COD_KEY = fact.SCHOOL_DAY_KEY
-        LEFT JOIN
-            world.dim_pc_usages dpu ON dpu.COD_KEY = fact.PC_USAGES_KEY
+        world.dim_pc_usages dpu ON dpu.COD_KEY = fact.PC_USAGES_KEY
